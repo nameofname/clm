8147803f8fc148f547d7bb8437a2abf13a34bcb9
nameofname <nameofemail@gmail.com>
2015-11-16 02:35:29 -0500

    I now have a letter that moves a little bit.

--- main.es.js
+++ main.es.js
@@ -2,16 +2,27 @@
 
 const drawFrame = require('./lib/drawFrame');
 const blankMatrix = require('./lib/blankMatrix');
-const a = require('./symbols/alphaNumeric/a');
+const clear = require('./lib/clear');
 const Symbol = require('./lib/Symbol');
-const m = blankMatrix();
+const a = require('./symbols/alphaNumeric/a');
 
 const aaa = new Symbol();
-aaa.register(a);
-aaa.update(m);
 
-drawFrame.start(m);
+aaa.register(a);
+
+
+
+let t;
+
+t = setInterval(() => {
+    const m = blankMatrix();
+    aaa.update(m);
+
+    clear();
+    drawFrame(m);
+}, 1000);
 
 setTimeout(() => {
-    drawFrame.stop();
+    clearInterval(t);
 }, 5000);
+
