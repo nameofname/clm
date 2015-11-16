d3a03928663b63fabfa90b43ba33892bb35124cf
nameofname <nameofemail@gmail.com>
2015-11-16 01:29:23 -0500

    perfectec A and 1

--- main.es.js
+++ main.es.js
@@ -2,7 +2,8 @@
 
 const drawFrame = require('./lib/drawFrame');
 const matrix = require('./lib/matrix');
-const one = require('./symbols/alphaNumeric/1');
+//const one = require('./symbols/alphaNumeric/1');
+const one = require('./symbols/alphaNumeric/a');
 
 // start the frames then stop after 5 seconds :
 drawFrame.start(matrix([one]));
