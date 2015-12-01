c1dee9f3b6850dab000db699ec6c97961f411d4d
nameofname <nameofemail@gmail.com>
2015-12-01 14:12:38 -0500

    resolved file extension issues. Code now works with babel require hook or transpiled.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,11 +1,8 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
-var extReplace = require('gulp-ext-replace');
-
 
 gulp.task("default", function () {
   return gulp.src("app/**/*.js")
     .pipe(babel({ presets: ['es2015'] }))
-    .pipe(extReplace('.js'))
     .pipe(gulp.dest("dist"));
 });
---
+++ test.js
@@ -0,0 +1,4 @@
+"use strict";
+
+require('./babelConf');
+module.exports = require('./dist/lib/Controller');
