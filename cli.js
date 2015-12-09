4580d99c19bc8f3cde12d7aa5f5c279380ad2faa
nameofname <nameofemail@gmail.com>
2015-12-09 11:35:46 -0500

    Beefed up the cli a little bit.

--- cli.js
+++ cli.js
@@ -1,27 +1,48 @@
 "use strict";
 
-require('./babelConf');
+var Controller = require('./main');
 var commander = require('commander');
-var _doMarquee = require('./prepared-methods/marquee.es');
+var controller = new Controller();
 
 var speed;
 var duration;
-var marquee;
+var message;
+var xOff;
+var yOff;
 
+// set up commander options
 commander
-    .option('-m, --marquee [value]', 'String to animate as a marquee')
+    .option('-m, --message [value]', 'String to animate as a message')
     .option('-s, --speed <n>', 'Speed of your animation', parseInt)
     .option('-d, --duration <n>', 'Duration of your animation in milliseconds', parseInt)
+    .option('-x, --xOffset <n>', 'X-offset for your symbol per frame', parseInt)
+    .option('-y, --yOffset <n>', 'Y-offset for your symbol per frame', parseInt)
     .parse(process.argv);
 
 if (process.argv.length < 2) {
     commander.outputHelp();
 }
 
-marquee = commander.marquee;
+// set variables based on commander input
+message = commander.message;
 speed = commander.speed;
-duration = commander.duration;
+xOff = commander.xOffset;
+yOff = commander.yOffset;
+duration = commander.duration || 5000;
 
-if (marquee) {
-    _doMarquee(marquee, speed, duration);
+// use passed variables
+if (message) {
+    controller.setString(message);
 }
+if (speed) {
+    controller.setSpeed(speed);
+}
+if (xOff) {
+    controller.setXOffset(xOff);
+}
+if (yOff) {
+    controller.setYOffset(yOff);
+}
+
+controller.setDuration(duration);
+controller.go();
