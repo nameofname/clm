9d5398e7a78c40dfe39044bf681e1d81023fc365
nameofname <nameofemail@gmail.com>
2015-12-12 12:43:30 -0500

    added gulp clean task

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,17 @@
+"use strict";
+
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var clean = require("gulp-clean");
+
 
 gulp.task("default", function () {
-  return gulp.src("app/**/*.js")
+
+    gulp.src('dist/*', {read: false})
+        .pipe(clean());
+
+    return gulp.src("app/**/*.js")
     .pipe(babel({ presets: ['es2015'] }))
     .pipe(gulp.dest("dist"));
+
 });
